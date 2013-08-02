85530179419b16757347aa03626aad554ac189c9
Martin Muñoz <im.mmun@gmail.com>
2013-08-02 02:11:03 -0400

    Initial commit

---
+++ mac/link.sh
@@ -0,0 +1,5 @@
+ln -s ~/Git/mmun/dotfiles/mac/bash_profile      ~/.bash_profile
+ln -s ~/Git/mmun/dotfiles/mac/bashrc            ~/.bashrc
+ln -s ~/Git/mmun/dotfiles/mac/gemrc             ~/.gemrc
+ln -s ~/Git/mmun/dotfiles/mac/gitconfig         ~/.gitconfig
+ln -s ~/Git/mmun/dotfiles/mac/gitignore         ~/.gitignore
---
+++ ubuntu-vm/link.sh
@@ -0,0 +1,5 @@
+ln -s /git/mmun/dotfiles/ubuntu-vm/bash_profile       ~/.bash_profile
+ln -s /git/mmun/dotfiles/ubuntu-vm/bashrc             ~/.bashrc
+ln -s /git/mmun/dotfiles/ubuntu-vm/gemrc              ~/.gemrc
+ln -s /git/mmun/dotfiles/ubuntu-vm/gitconfig          ~/.gitconfig
+ln -s /git/mmun/dotfiles/ubuntu-vm/gitignore          ~/.gitignore
